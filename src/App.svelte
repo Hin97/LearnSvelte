08f1fbe852d87acba60e8b582a7c4fa42278b1b4
Rongxuan Li <48315282+Hin97@users.noreply.github.com>
2023-08-28 17:15:45 +1000

    Finish base syntax section

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
+	import ContactCard from './ContactCaqrd.svelte'
+
 	let name = "Jack";
+	let title = ""
+	let imageUrl=""
+	let description = ""
 	let age = 26;
 
 	$:uppercaseName = name.toUpperCase()
@@ -35,4 +40,15 @@
 <button on:click="{incrementAge}" >Change Age</button>
 <!-- <button on:click="{changeName}">Change Name</button> -->
 <!-- <input type="text" value="{name}" on:input={nameInput}> -->
-<input type="text" bind:value="{name}">
+
+<input type="text" bind:value="{name}" placeholder="name"/>
+<input type="text" bind:value="{title}"
+placeholder="title"/>
+<input type="text" bind:value="{imageUrl}"
+placeholder="imageUrl"/>
+<textarea row="3" bind:value="{description}"></textarea>
+<ContactCard
+userName={name}
+jobTitle={title} 
+{description}
+imagePath = {imageUrl}/>
