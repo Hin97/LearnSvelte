183bceb61b0d8ad6a8232e6929245e307bc491ff
Rongxuan Li <48315282+Hin97@users.noreply.github.com>
2023-09-06 12:52:37 +1000

    Finish Assignment 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,85 @@
 <script>
-	import ContactCard from './ContactCard.svelte'
+  import ContactCard from "./ContactCard.svelte";
 
-	let name = "Jack";
-	let title = ""
-	let imageUrl=""
-	let description = ""
-	let age = 26;
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formstate = "empty";
 
-	$:uppercaseName = name.toUpperCase()
+  let createdContacts = [];
 
-	$: console.log(name);
+  function addContact(){
+    if (name.trim().length == 0 || 
+    title.trim().length == 0 ||
+    image.trim().length == 0 ||
+    description.trim().length == 0){
+      formstate = "invalid";
+      return;
+    }
+    createdContacts = [...createdContacts,{
+      id: Math.random(),
+      name: name,
+      jobTitle: title,
+      imageUrl: image,
+      desc: description
+    }]
+    formstate = "done";
+  }
 
-	$: if(name === "JackLi"){
-		console.log("It runs")
-		age = 31;
-	}
+  function deleteFirst(){
+    createdContacts = createdContacts.slice(1)
+  }
 
-	function incrementAge(){
-		age += 1;
-	}
-
-	function changeName(){
-		name = "JackLi"
-	}
-
-	function nameInput(event){
-		const enteredValue = event.target.value
-		name = enteredValue
-	}
+  function deleteLast(){
+    createdContacts = createdContacts.slice(0, -1)
+  } 
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
 </style>
 
-<h1>Hello {uppercaseName}! My age is {age}!</h1>
-<button on:click="{incrementAge}" >Change Age</button>
-<!-- <button on:click="{changeName}">Change Name</button> -->
-<!-- <input type="text" value="{name}" on:input={nameInput}> -->
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
 
-<input type="text" bind:value="{name}" placeholder="name"/>
-<input type="text" bind:value="{title}"
-placeholder="title"/>
-<input type="text" bind:value="{imageUrl}"
-placeholder="imageUrl"/>
-<textarea row="3" bind:value="{description}"></textarea>
-<ContactCard
-userName={name}
-jobTitle={title} 
-{description}
-imagePath = {imageUrl}/>
+<button on:click={addContact}>Add Contact Card</button>
+<button on:click={deleteFirst}>Delete First</button>
+<button on:click={deleteLast}>Delete Last</button>
+
+
+{#if formstate === "invalid"}
+<p>Invalid input</p>
+{:else}
+<p>Please input something and click the button</p>
+{/if}
+
+{#each createdContacts as contact, i (contact.id)}
+<h2># {i+1}</h2>
+<ContactCard 
+userName = {contact.name} 
+jobTitle = {contact.jobTitle} 
+description = {contact.desc}
+userImage = {contact.imageUrl} />
+{:else}
+<p>Please start adding some contacts, we found none!</p>
+{/each}
---
+++ src/Assignment2/App.svelte
@@ -0,0 +1,44 @@
+<h1>Assignment</h1>
+
+<ol>
+	<li>Add a password input field and save the user input in a variable.</li>
+	<li>Output "Too short" if the password is shorter than 5 characters and "Too long" if it's longer than 10.</li>
+	<li>Output the password in a paragraph tag if it's between these boundaries.</li>
+	<li>Add a button and let the user add the passwords to an array.</li>
+	<li>Output the array values (= passwords) in a unordered list (ul tag).</li>
+	<li>Bonus: If a password is clicked, remove it from the list.</li>
+</ol>
+
+<script>
+	let password= ""
+	let passwords = [];
+
+	function addPassword(){
+		passwords = [...passwords, password]
+	}
+
+	function removePassword(index){
+		passwords = passwords.filter((pw,idx) => {
+			return idx !== index;
+		})
+	}
+
+</script>
+
+<input type="password" bind:value={password} id="password" />
+
+{#if password.trim().length < 5}
+	<p>Too short</p>
+{:else if password.trim().length > 10}
+	<p>Too long</p>
+{:else}
+	<p>Password: {password}</p>
+	<button on:click={addPassword}>Add password</button>
+{/if}
+
+<ul>
+{#each passwords as password, index}
+	<li on:click={removePassword.bind(this, index)}>{password}</li>
+
+{/each}
+</ul> 
